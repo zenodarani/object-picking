0909699cf9ad131c97cbf7a776a9dba94724016a
Cristian704 <120493260+Cristian704@users.noreply.github.com>
2024-11-19 10:05:55 +0100

    Added threshold of target and template masks

--- src/recognition.py
+++ src/recognition.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-def recognition(target_path, template_path, match_threshold=0.1, contour_error = 10):
+def recognition(target_path, template_path, match_thresh=0.1, contour_error = 10, template_thresh = 120, target_thresh=120):
     target = cv2.imread(target_path)
 
     with np.load('intrinsics.npz') as item:
@@ -16,13 +16,13 @@ def recognition(target_path, template_path, match_threshold=0.1, contour_error =
     template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 
 
-    _, target_thresh = cv2.threshold(target_gray, 120, 255, cv2.THRESH_BINARY)
-    _, template_thresh = cv2.threshold(template_gray, 120, 255, cv2.THRESH_BINARY)
+    _, target_thresh = cv2.threshold(target_gray, target_thresh, 255, cv2.THRESH_BINARY)
+    _, template_thresh = cv2.threshold(template_gray, template_thresh, 255, cv2.THRESH_BINARY)
 
     target_contours, _ = cv2.findContours(target_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     template_contours, _ = cv2.findContours(template_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    template_contour = template_contours[0]
+    template_contour = max(template_contours, key=lambda c: c.shape[0])
 
     target_with_contours = undistorted_target.copy()
     cv2.drawContours(target_with_contours, target_contours, -1, (0, 0, 255), 3)
@@ -32,7 +32,8 @@ def recognition(target_path, template_path, match_threshold=0.1, contour_error =
     template_contour_length = cv2.arcLength(template_contour, True)
     for c in target_contours:
         match = cv2.matchShapes(template_contour, c, 3, 0)
-        if match <= match_threshold and template_contour_length - contour_error <= cv2.arcLength(c,True) <= template_contour_length + contour_error:
+        print(match)
+        if match <= match_thresh and template_contour_length - contour_error <= cv2.arcLength(c,True) <= template_contour_length + contour_error:
             valid_matches.append(c)
     target_matched = undistorted_target.copy()
     cv2.drawContours(target_matched, valid_matches, -1, (0, 255, 0), 3)
@@ -78,5 +79,14 @@ def recognition(target_path, template_path, match_threshold=0.1, contour_error =
 
 
 if __name__ == '__main__':
-    contours, means, eigenvectors = recognition('../object_images/tapes_and_pipes_50ms.png', '../template_images/tape_template.png', match_threshold=0.1, contour_error=10)
-    # contours, means, eigenvectors = recognition('../object_images/detection_tryal.png', '../template_images/almond_template.png', match_threshold=0.5, contour_error=20)
+    # contours, means, eigenvectors = recognition('../object_images/tapes_and_pipes_50ms.png', '../template_images/tape_template.png', match_thresh=0.005, contour_error=10)
+    # contours, means, eigenvectors = recognition('../object_images/detection_tryal.png', '../template_images/tape_template.png', match_thresh=0.005, contour_error=20)
+    # contours, means, eigenvectors = recognition('../object_images/all_100ms.png', '../template_images/hook_template.png', match_thresh=3, contour_error=95)
+    # contours, means, eigenvectors = recognition('../object_images/perfumes_and_batteries_100ms.png', '../template_images/battery_template.png', match_thresh=0.5, contour_error=300)
+
+
+
+
+
+    # contours, means, eigenvectors = recognition('../object_images/markers_and_valves_100ms.png', '../template_images/marker_template.png', match_thresh=1, contour_error=300, target_thresh=25, template_thresh=50)
+    contours, means, eigenvectors = recognition('../object_images/hooks_and_caps_100ms.png', '../template_images/cap_template.png', match_thresh=3, contour_error=100, target_thresh=25, template_thresh=50)
